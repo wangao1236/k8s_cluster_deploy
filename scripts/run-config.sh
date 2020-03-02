69108aa509a147f07a47ae66e13998a23252f01c
wangao1236 <908622977@qq.com>
2020-03-02 19:06:11 +0800

    create

--- scripts/run-config.sh
+++ scripts/run-config.sh
@@ -1,39 +1,90 @@
 #!/bin/bash
 
+UPDATE=0
+NGINX=0
+TEST=0
+
+while [[ $# -ge 1 ]]; do
+    case $1 in
+        -u|--update )
+            UPDATE=$2
+            shift 2
+            ;;
+        -n|--nginx )
+            NGINX=$2
+            shift 2
+            ;;
+        -t|--test )
+            TEST=$2
+            shift 2
+            ;;
+    esac
+done
+
+echo "UPDATE=$UPDATE; NGINX=$NGINX; TEST=$TEST"
+
 #------------- update cert && kubeconfig
-echo -e "\033[32m ======>>>>>>delete old cert && kubeconfig \033[0m"
-rm -rf ../k8s-cert/*
-rm -rf ../kubeconfig/*
-sudo rm -rf /opt/kubernetes/ssl/*
-ssh root@master2 rm -rf /opt/kubernetes/ssl/*
-ssh root@master3 rm -rf /opt/kubernetes/ssl/*
-echo -e "\033[32m ======>>>>>>generate new cert \033[0m"
-cp k8s-cert.sh ../k8s-cert
-cd ../k8s-cert
-./k8s-cert.sh
-echo -e "\033[32m ======>>>>>>copy new cert \033[0m"
-sudo cp -r ca* admin* master node /opt/kubernetes/ssl
-sudo scp -r /opt/kubernetes/ssl root@master2:/opt/kubernetes/
-sudo scp -r /opt/kubernetes/ssl root@master3:/opt/kubernetes/
-echo -e "\033[32m ======>>>>>>generate new kubeconfig \033[0m"
-cd ../scripts
-sudo rm -rf /opt/kubernetes/cfg/*
-ssh root@master2 rm -rf /opt/kubernetes/cfg/*
-ssh root@master3 rm -rf /opt/kubernetes/cfg/*
-cp kubeconfig.sh ../kubeconfig
-cd ../kubeconfig
-sudo ./kubeconfig.sh 192.168.1.65 /opt/kubernetes/ssl
-echo -e "\033[32m ======>>>>>>copy new kubeconfig \033[0m"
-sudo cp * /opt/kubernetes/cfg
-sudo scp -r /opt/kubernetes/cfg root@master2:/opt/kubernetes/
-sudo scp -r /opt/kubernetes/cfg root@master3:/opt/kubernetes/
-sudo chown ao:ao config
-cp config /home/ao/.kube/
-scp config ao@master2:/home/ao/.kube/
-scp config ao@master3:/home/ao/.kube/
-cd ../scripts
+function update_cert_kubeconfig() {
+    echo -e "\033[32m ======>>>>>>delete old cert && kubeconfig \033[0m"
+    rm -rf ../k8s-cert/*
+    rm -rf ../kubeconfig/*
+    sudo rm -rf /opt/kubernetes/ssl/*
+    ssh root@master2 rm -rf /opt/kubernetes/ssl/*
+    ssh root@master3 rm -rf /opt/kubernetes/ssl/*
+    echo -e "\033[32m ======>>>>>>generate new cert \033[0m"
+    cp k8s-cert.sh ../k8s-cert
+    cd ../k8s-cert
+    ./k8s-cert.sh
+    echo -e "\033[32m ======>>>>>>copy new cert \033[0m"
+    sudo cp -r ca* admin* master node /opt/kubernetes/ssl
+    sudo scp -r /opt/kubernetes/ssl root@master2:/opt/kubernetes/
+    sudo scp -r /opt/kubernetes/ssl root@master3:/opt/kubernetes/
+    echo -e "\033[32m ======>>>>>>generate new kubeconfig \033[0m"
+    cd ../scripts
+    sudo rm -rf /opt/kubernetes/cfg/*
+    ssh root@master2 rm -rf /opt/kubernetes/cfg/*
+    ssh root@master2 rm -rf /opt/kubernetes/cfg/*
+    cp kubeconfig.sh ../kubeconfig
+    cd ../kubeconfig
+    sudo ./kubeconfig.sh 192.168.1.65 /opt/kubernetes/ssl
+    echo -e "\033[32m ======>>>>>>copy new kubeconfig \033[0m"
+    sudo cp * /opt/kubernetes/cfg
+    sudo scp -r /opt/kubernetes/cfg root@master2:/opt/kubernetes/
+    sudo scp -r /opt/kubernetes/cfg root@master3:/opt/kubernetes/
+    sudo chown ao:ao config
+    cp config /home/ao/.kube/
+    scp config ao@master2:/home/ao/.kube/
+    scp config ao@master3:/home/ao/.kube/
+    cd ../scripts
+}
+
+if [ $UPDATE -eq 1 ]; then
+    update_cert_kubeconfig
+fi
+
 #------------- restart components
+echo -e "\033[32m ======>>>>>>restart haproxy \033[0m"
+if [ $UPDATE -eq 1 ]; then
+    ssh root@lb1 "systemctl stop nginx.service && \
+        systemctl start haproxy.service"
+fi
+
+if [ $TEST -eq 1 ]; then
+    sudo scp -r /opt/kubernetes/ssl/* root@lb1:/etc/nginx/ssl/
+    ssh root@lb1 "cd /etc/nginx/ssl && \
+        cat admin.pem > test.pem && \
+        cat admin-key.pem > test-key.pem && \
+        systemctl stop haproxy.service && \
+        systemctl daemon-reload && \
+        systemctl restart nginx.service && \
+        systemctl status nginx.service"
+fi
+
 echo -e "\033[32m ======>>>>>>restart etcd \033[0m"
+sudo systemctl stop etcd.service
+ssh root@master2 "systemctl stop etcd.service"
+ssh root@master3 "systemctl stop etcd.service"
+sleep 10s
 sudo rm -rf /var/lib/etcd/default.etcd/member
 ssh root@master2 "rm -rf /var/lib/etcd/default.etcd/member"
 ssh root@master3 "rm -rf /var/lib/etcd/default.etcd/member"
@@ -152,8 +203,58 @@ kubectl get csr
 
 echo -e "\033[32m ======>>>>>>check nodes \033[0m"
 sleep 10s
+echo ">>>>>>>>>>>>>>>>>>>1"
+ssh root@lb1 "cd /etc/nginx/ssl && \
+    rm -rf *"
+echo ">>>>>>>>>>>>>>>>>>>2"
+sudo scp -r /opt/kubernetes/ssl/* root@lb1:/etc/nginx/ssl/
+echo ">>>>>>>>>>>>>>>>>>>3"
+ssh root@lb1 "ls -l /etc/nginx/ssl/"
+echo ">>>>>>>>>>>>>>>>>>>4"
 echo "1st"
-kubectl get nodes
+cmd_check_node=$(kubectl get nodes | grep -E -o 'not found')
+not_found_array=(${cmd_check_node//,/ })
+while [ ${#not_found_array[@]} -gt 0 ]
+do
+    echo -e "\033[31m !!!!!!!! Waiting for node to be ready \033[0m"
+    sleep 3s
+    cmd_check_node=$(kubectl get nodes | grep -E -o 'not found')
+    not_found_array=(${cmd_check_node//,/ })
+done
+
+sleep 10s
+
+if [ $NGINX -eq 1 ]; then
+    #while [ ! -f "/opt/kubernetes/ssl/node/kubelet-client-current.pem" ]; do
+    #    sleep 3s
+    #    echo -e "\033[31m Waiting for file named '/opt/kubernetes/ssl/node/kubelet-client-current.pem' to be made \033[0m"
+    #done
+    echo -e "\033[32m ======>>>>>>restart nginx \033[0m"
+    ssh root@lb1 "cd /etc/nginx/ssl && \
+        rm -rf *"
+    sudo scp -r /opt/kubernetes/ssl/* root@lb1:/etc/nginx/ssl/
+    ssh root@lb1 "ls -l /etc/nginx/ssl/"
+    ssh root@lb1 "cd /etc/nginx/ssl && \
+        cat admin.pem > test.pem && \
+        cat admin-key.pem > test-key.pem && \
+        systemctl stop haproxy.service && \
+        systemctl daemon-reload && \
+        systemctl restart nginx.service && \
+        systemctl status nginx.service"
+#    ssh root@lb1 "cd /etc/nginx/ssl && \
+#        cat admin.pem > test.pem && \
+#        cat node/kubelet-client-current.pem |head -n 17 | tail -n +1 >> test.pem >> test.pem && \
+#        cat admin-key.pem > test-key.pem && \
+#        cat node/kubelet-client-current.pem |head -n 22 | tail -n +18 >> test-key.pem && \
+#        systemctl stop haproxy.service && \
+#        systemctl daemon-reload && \
+#        systemctl restart nginx.service && \
+#        systemctl status nginx.service"
+fi
+
+echo -e "\033[32m ======>>>>>>add nodes \033[0m"
+kubectl get csr
+sleep 10s
 kubectl label node node1 node2 node3 node-role.kubernetes.io/master=true
 echo "2nd"
 kubectl get nodes
