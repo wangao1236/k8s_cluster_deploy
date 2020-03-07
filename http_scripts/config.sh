95ff48ded8ee9c0effc8aaf8f952cd487514313f
wangao1236 <908622977@qq.com>
2020-03-07 11:58:57 +0800

    update http_scripts

--- http_scripts/config.sh
+++ http_scripts/config.sh
@@ -3,7 +3,9 @@
 ETCD_SERVERS=$1
 APISERVER=$2
 
-cat <<EOF >config
+export KUBE_APISERVER="http://$APISERVER:8443"
+
+cat <<EOF >global
 ###
 # kubernetes system config
 #
@@ -16,48 +18,16 @@ cat <<EOF >config
 #   kubelet.service
 #   kube-proxy.service
 # logging to stderr means we get it in the systemd journal
-KUBE_LOGTOSTDERR="--logtostderr=true"
+KUBE_LOGTOSTDERR="--logtostderr=false"
  
 # journal message level, 0 is debug
-KUBE_LOG_LEVEL="--v=0"
+KUBE_LOG_LEVEL="--v=8"
  
 # Should this cluster be allowed to run privileged docker containers
 KUBE_ALLOW_PRIV="--allow-privileged=false"
  
 # How the controller-manager, scheduler, and proxy find the apiserver
 KUBE_MASTER="--master=http://$APISERVER:8443"
-EOF
-
-cat <<EOF >apiserver
-###
-# kubernetes system config
-#
-# The following values are used to configure the kube-apiserver
-#
-# The address on the local server to listen to.
-KUBE_API_ADDRESS="--insecure-bind-address=0.0.0.0"
-
-# The port on the local server to listen on.
-# KUBE_API_PORT="--port=8080"
-
-# Port minions listen on
-# KUBELET_PORT="--kubelet-port=10250"
-
-# Comma separated list of nodes in the etcd cluster
-KUBE_ETCD_SERVERS="--etcd-servers=${ETCD_SERVERS} \\
-    --etcd-cafile=/opt/etcd/ssl/ca.pem \\
-    --etcd-certfile=/opt/etcd/ssl/server.pem \\
-    --etcd-keyfile=/opt/etcd/ssl/server-key.pem"
-
-# Address range to use for services
-KUBE_SERVICE_ADDRESSES="--service-cluster-ip-range=10.254.0.0/16"
-
-# default admission control policies
-#KUBE_ADMISSION_CONTROL="--admission-control=NamespaceLifecycle,NamespaceExists,LimitRanger,SecurityContextDeny,ServiceAccount,ResourceQuota"
-KUBE_ADMISSION_CONTROL="--admission-control=NamespaceLifecycle,NamespaceExists,LimitRanger,SecurityContextDeny,ResourceQuota"
-
-# Add your own!
-KUBE_API_ARGS=""
 EOF
 
 cat <<EOF >controller-manager
@@ -67,48 +37,40 @@ cat <<EOF >controller-manager
 # defaults from config and apiserver should be adequate
  
 # Add your own!
-KUBE_CONTROLLER_MANAGER_ARGS=""
+KUBE_CONTROLLER_MANAGER_ARGS="--log-file=/opt/kubernetes/log/controller-manager.log"
 EOF
 
-cat <<EOF >scheduler
+cat <<EOF >/opt/kubernetes/cfg/scheduler
 ###
 # kubernetes scheduler config
  
 # default config should be adequate
  
 # Add your own!
-KUBE_SCHEDULER_ARGS="--loglevel=0"
+KUBE_SCHEDULER_ARGS="--log-file=/opt/kubernetes/log/scheduler.log"
 EOF
 
-cat <<EOF >kubelet
-###
-# kubernetes kubelet (minion) config
- 
-# The address for the info server to serve on (set to 0.0.0.0 or "" for all interfaces)
-# KUBELET_ADDRESS="--address=127.0.0.1"
- 
-# The port for the info server to serve on
-KUBELET_PORT="--port=10250"
- 
-# You may leave this blank to use the actual hostname
-# KUBELET_HOSTNAME="--hostname-override=127.0.0.1"
- 
-# location of the api-server
-##KUBELET_API_SERVER="--api-servers=http://127.0.0.1:8080"
- 
-# pod infrastructure container
-KUBELET_POD_INFRA_CONTAINER="--pod-infra-container-image=docker.io/kubernetes/pause"
- 
-# Add your own!
-KUBELET_ARGS="--fail-swap-on=false --cgroup-driver=cgroupfs --kubeconfig=/var/lib/kubelet/kubeconfig"
-EOF
+kubectl config set-cluster kubernetes \
+  --server=${KUBE_APISERVER} \
+  --embed-certs=false \
+  --kubeconfig=config
 
-cat <<EOF >proxy
-###
-# kubernetes proxy config
- 
-# default config should be adequate
- 
-# Add your own!
-KUBE_PROXY_ARGS=""
-EOF
+kubectl config set-context default \
+  --cluster=kubernetes \
+  --user="" \
+  --kubeconfig=config
+
+kubectl config use-context default --kubeconfig=config
+
+kubectl config set-cluster kubernetes \
+  --embed-certs=false \
+  --server=${KUBE_APISERVER} \
+  --kubeconfig=kubelet.kubeconfig
+
+kubectl config set-context default \
+  --cluster=kubernetes \
+  --user="" \
+  --kubeconfig=kubelet.kubeconfig
+
+# 设置默认上下文
+kubectl config use-context default --kubeconfig=kubelet.kubeconfig
--- http_scripts/install.sh
+++ http_scripts/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo mkdir -p /opt/kubernetes/{bin,cfg,log}
 sudo rm -rf /opt/kubernetes/cfg/*
 sudo rm -rf /opt/kubernetes/log/*
@@ -9,26 +11,30 @@ ssh root@master3 "mkdir -p /opt/kubernetes/{bin,cfg,log} && \
     rm -rf /opt/kubernetes/log/*"
 
 mkdir -p ../config
-rm ../config/*
-ssh root@master2 "rm /opt/kubernetes/cfg/*"
-ssh root@master3 "rm /opt/kubernetes/cfg/*"
+sudo rm -rf ../config/*
+ssh root@master2 "rm -rf /opt/kubernetes/cfg/*"
+ssh root@master3 "rm -rf /opt/kubernetes/cfg/*"
 cp config.sh ../config
 cd ../config
-./config.sh https://192.168.1.67:2379,https://192.168.1.68:2379,https://192.168.1.69:2379 192.168.1.66
+sudo ./config.sh https://192.168.1.67:2379,https://192.168.1.68:2379,https://192.168.1.69:2379 192.168.1.66
 sudo cp * /opt/kubernetes/cfg
+sudo chown ao:ao config
+cp config ~/.kube/
 sudo scp /opt/kubernetes/cfg/* root@master2:/opt/kubernetes/cfg/
+scp config ao@master2:/home/ao/.kube/
 sudo scp /opt/kubernetes/cfg/* root@master3:/opt/kubernetes/cfg/
-cd ../http_script
+scp config ao@master3:/home/ao/.kube/
+cd ../http_scripts
 
 echo -e "\033[32m ======>>>>>>restart etcd \033[0m"
 sudo systemctl stop etcd.service
 ssh root@master2 "systemctl stop etcd.service"
 ssh root@master3 "systemctl stop etcd.service"
-sleep 10s
+sleep 5s
 sudo rm -rf /var/lib/etcd/default.etcd/member
 ssh root@master2 "rm -rf /var/lib/etcd/default.etcd/member"
 ssh root@master3 "rm -rf /var/lib/etcd/default.etcd/member"
-sleep 10s
+sleep 5s
 sudo systemctl daemon-reload
 sudo systemctl restart etcd.service
 ssh root@master2 "systemctl daemon-reload && \
@@ -50,7 +56,6 @@ ssh root@master2 "hostname && \
     systemctl daemon-reload && \
     systemctl restart docker && \
     systemctl status docker"
-
 ssh root@master3 "hostname && \
     cd /home/ao/Coding/k8s/scripts && \
     ./flannel.sh https://192.168.1.67:2379,https://192.168.1.68:2379,https://192.168.1.69:2379 && \
@@ -58,16 +63,18 @@ ssh root@master3 "hostname && \
     systemctl restart docker && \
     systemctl status docker"
 
-./apiserver.sh
-scp apiserver.sh ao@master2:/home/ao/Coding/k8s/http_scripts && scp apiserver.sh ao@master3:/home/ao/Coding/http_k8s/scripts
+echo -e "\033[32m ======>>>>>>restart kube-apiserver \033[0m"
+sudo ./apiserver.sh 192.168.1.67 https://192.168.1.67:2379,https://192.168.1.68:2379,https://192.168.1.69:2379
+scp apiserver.sh ao@master2:/home/ao/Coding/k8s/http_scripts && scp apiserver.sh ao@master3:/home/ao/Coding/k8s/http_scripts
 ssh root@master2 "hostname && \
     cd /home/ao/Coding/k8s/http_scripts && \
-    ./apiserver.sh"
+    ./apiserver.sh 192.168.1.68 https://192.168.1.67:2379,https://192.168.1.68:2379,https://192.168.1.69:2379"
 ssh root@master3 "hostname && \
     cd /home/ao/Coding/k8s/http_scripts && \
-    ./apiserver.sh"
+    ./apiserver.sh 192.168.1.69 https://192.168.1.67:2379,https://192.168.1.68:2379,https://192.168.1.69:2379"
 
-./controller.sh
+echo -e "\033[32m ======>>>>>>restart kube-controller-manager \033[0m"
+sudo ./controller-manager.sh
 scp controller-manager.sh ao@master2:/home/ao/Coding/k8s/http_scripts && scp controller-manager.sh ao@master3:/home/ao/Coding/k8s/http_scripts
 ssh root@master2 "hostname && \
     cd /home/ao/Coding/k8s/http_scripts && \
@@ -76,7 +83,8 @@ ssh root@master3 "hostname && \
     cd /home/ao/Coding/k8s/http_scripts && \
     ./controller-manager.sh"
 
-./scheduler.sh
+echo -e "\033[32m ======>>>>>>restart kube-scheduler \033[0m"
+sudo ./scheduler.sh
 scp scheduler.sh ao@master2:/home/ao/Coding/k8s/http_scripts && scp scheduler.sh ao@master3:/home/ao/Coding/k8s/http_scripts
 ssh root@master2 "hostname && \
     cd /home/ao/Coding/k8s/http_scripts && \
@@ -85,7 +93,8 @@ ssh root@master3 "hostname && \
     cd /home/ao/Coding/k8s/http_scripts && \
     ./scheduler.sh"
 
-./kubelet.sh 192.168.1.67 node1
+echo -e "\033[32m ======>>>>>>restart kubelet \033[0m"
+sudo ./kubelet.sh 192.168.1.67 node1
 scp kubelet.sh ao@master2:/home/ao/Coding/k8s/http_scripts && scp kubelet.sh ao@master3:/home/ao/Coding/k8s/http_scripts
 ssh root@master2 "hostname && \
     cd /home/ao/Coding/k8s/http_scripts && \
@@ -94,7 +103,8 @@ ssh root@master3 "hostname && \
     cd /home/ao/Coding/k8s/http_scripts && \
     ./kubelet.sh 192.168.1.69 node3"
 
-./proxy.sh node1
+echo -e "\033[32m ======>>>>>>restart proxy \033[0m"
+sudo ./proxy.sh node1
 scp proxy.sh ao@master2:/home/ao/Coding/k8s/http_scripts && scp proxy.sh ao@master3:/home/ao/Coding/k8s/http_scripts
 ssh root@master2 "hostname && \
     cd /home/ao/Coding/k8s/http_scripts && \
@@ -102,3 +112,16 @@ ssh root@master2 "hostname && \
 ssh root@master3 "hostname && \
     cd /home/ao/Coding/k8s/http_scripts && \
     ./proxy.sh node3"
+
+echo "1st"
+sleep 10s
+kubectl label node node1 node2 node3 node-role.kubernetes.io/master=true
+echo "2nd"
+kubectl get nodes --all-namespaces
+kubectl taint nodes --all node-role.kubernetes.io/master=true:NoSchedule
+kubectl taint nodes --all node-role.kubernetes.io/master-
+sleep 10s
+echo "3rd"
+kubectl get nodes --all-namespaces
+kubectl apply -f ../yamls//nginx-deployment.yaml
+kubectl get pods --all-namespaces
